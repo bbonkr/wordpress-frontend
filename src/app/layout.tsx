4c078d53c4b923ec065b4067f4f8debfff3a7b70
Pon Cheol Ku (구본철) <bbon@live.com>
2025-02-26 16:58:20 +0900

    feat: Root route #1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,17 +10,19 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default async function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   const { isEnabled } = await draftMode();
 
   return (
     <html lang="en">
       <body className={inter.className}>
-        {isEnabled && <PreviewNotice />}
-        <Navigation />
-        {children}
+        <div className="flex flex-col min-h-screen">
+          {isEnabled && <PreviewNotice />}
+          <Navigation />
+          {children}
+        </div>
       </body>
     </html>
   );
--- src/components/Templates/PostList/PostListTemplate.tsx
+++ src/components/Templates/PostList/PostListTemplate.tsx
@@ -1,27 +1,46 @@
 import { print } from "graphql/language/printer";
 
-import { Post, PostConnection } from "@/gql/graphql";
+import { PostConnection } from "@/gql/graphql";
 import { fetchGraphQL } from "@/utils/fetchGraphQL";
 
 import styles from "./PostListTemplate.module.css";
 import { PostListQuery } from "./PostListQuery";
 import Link from "next/link";
 
-interface TemplateProps {
-  page?: number;
-  limit?: number;
+interface PostListTemplate {
+  after?: string;
+  before?: string;
+  first?: string;
+  last?: string;
+  s?: string;
 }
 
-export default async function PostListTemplate({ page, limit }: TemplateProps) {
+const FIRST = 10;
+const LAST = 10;
+
+export default async function PostListTemplate({
+  after,
+  before,
+  first,
+  last,
+  s,
+}: Readonly<PostListTemplate>) {
+  const firstValue = !after && !before && !last ? `${FIRST}` : first;
+
   const { posts } = await fetchGraphQL<{
     posts: PostConnection;
-  }>(print(PostListQuery), {});
+  }>(print(PostListQuery), {
+    after: after,
+    before: before,
+    first: firstValue ? parseInt(firstValue, 10) : undefined,
+    last: last ? parseInt(last, 10) : undefined,
+    s: s,
+  });
 
   return (
-    <div>
+    <div className={styles.post}>
       <ul>
         {posts?.nodes.map((node) => {
-          console.info(node.title);
           return (
             <li key={node.id}>
               <Link href={`/${node.slug}`}>
@@ -31,6 +50,46 @@ export default async function PostListTemplate({ page, limit }: TemplateProps) {
           );
         })}
       </ul>
+      <hr />
+      <aside className="flex flex-col justify-center items-center py-3">
+        <ul className="flex flex-row gap-3">
+          <li>
+            {posts.pageInfo.hasPreviousPage ? (
+              <Link href="/">First</Link>
+            ) : (
+              <span className="cursor-not-allowed">First</span>
+            )}
+          </li>
+          <li>
+            {posts.pageInfo.hasPreviousPage ? (
+              <Link
+                href={`/?before=${posts.pageInfo.startCursor}&last=${LAST}`}
+              >
+                Previous ({posts.pageInfo.hasPreviousPage ? "✅" : "❌"})
+              </Link>
+            ) : (
+              <span className="cursor-not-allowed">Previous</span>
+            )}
+          </li>
+
+          <li>
+            {posts.pageInfo.hasNextPage ? (
+              <Link href={`/?after=${posts.pageInfo.endCursor}&first=${FIRST}`}>
+                Next ({posts.pageInfo.hasNextPage ? "✅" : "❌"})
+              </Link>
+            ) : (
+              <span className="cursor-not-allowed">Next</span>
+            )}
+          </li>
+          <li>
+            {posts.pageInfo.hasNextPage ? (
+              <Link href={`/?last=${LAST}`}>Last</Link>
+            ) : (
+              <span className="cursor-not-allowed">Last</span>
+            )}
+          </li>
+        </ul>
+      </aside>
     </div>
   );
 }
